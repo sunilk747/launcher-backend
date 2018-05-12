c0965c3eacc2fc32c56c0ab6357cf880be0a70fc
sunilk747 <sunilk747@gmail.com>
2018-05-12 18:16:45 +0530

    ajax change

--- web/src/main/java/io/fabric8/launcher/web/endpoints/websocket/MissionControlStatusEndpoint.java
+++ web/src/main/java/io/fabric8/launcher/web/endpoints/websocket/MissionControlStatusEndpoint.java
@@ -1,29 +1,32 @@
 package io.fabric8.launcher.web.endpoints.websocket;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import javax.enterprise.context.Dependent;
 import javax.enterprise.event.Observes;
 import javax.json.Json;
+import javax.json.JsonArray;
 import javax.json.JsonArrayBuilder;
 import javax.json.JsonObjectBuilder;
-import javax.websocket.OnClose;
-import javax.websocket.OnOpen;
-import javax.websocket.RemoteEndpoint;
-import javax.websocket.Session;
-import javax.websocket.server.PathParam;
-import javax.websocket.server.ServerEndpoint;
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Path;
+import javax.ws.rs.GET;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
 
 import io.fabric8.launcher.base.JsonUtils;
 import io.fabric8.launcher.core.api.events.StatusEventType;
 import io.fabric8.launcher.core.api.events.StatusMessageEvent;
+//import org.immutables.value.internal.$processor$.encode.$Type;
 
 /**
  * A websocket based resource that informs clients about the status of the operations
@@ -31,38 +34,44 @@ import io.fabric8.launcher.core.api.events.StatusMessageEvent;
  * https://abhirockzz.wordpress.com/2015/02/10/integrating-cdi-and-websockets/
  */
 @Dependent
-@ServerEndpoint(value = "/status/{uuid}")
+@Path("/status")
 public class MissionControlStatusEndpoint {
-    private static final Logger LOG = Logger.getLogger(MissionControlStatusEndpoint.class.getName());
 
-    private static final Map<UUID, Session> peers = new ConcurrentHashMap<>();
+    private static final Logger LOG = Logger.getLogger(MissionControlStatusEndpoint.class.getName());
 
     private static final Map<UUID, List<String>> messageBuffer = new ConcurrentHashMap<>();
 
-    @OnOpen
-    public void onOpen(Session session, @PathParam("uuid") String uuid) {
-        UUID key = UUID.fromString(uuid);
-        peers.put(key, session);
+    @Context
+    HttpServletResponse response;
+
+    @GET
+    @Path("/")
+    @Produces(MediaType.APPLICATION_JSON)
+    public JsonArray init() {
         JsonArrayBuilder builder = Json.createArrayBuilder();
         for (StatusEventType statusEventType : StatusEventType.values()) {
             JsonObjectBuilder object = Json.createObjectBuilder();
             builder.add(object.add(statusEventType.name(), statusEventType.getMessage()).build());
         }
 
-        RemoteEndpoint.Async asyncRemote = session.getAsyncRemote();
-        asyncRemote.sendText(builder.build().toString());
-        // Send pending messages
-        List<String> messages = messageBuffer.remove(key);
-        if (messages != null) {
-            messages.forEach(asyncRemote::sendText);
-        }
+        return builder.build();
     }
 
-    @OnClose
-    public void onClose(@PathParam("uuid") String uuid) {
-        UUID key = UUID.fromString(uuid);
-        peers.remove(key);
-        messageBuffer.remove(key);
+    @GET
+//    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/{uuid}")
+    public void message(@PathParam("uuid") String uuid) {
+        try {
+            PrintWriter wr = response.getWriter();
+            response.setContentType("application/json");
+            UUID key = UUID.fromString(uuid);
+            List<String> messages = messageBuffer.get(key);
+            wr.write(messages.toString());
+            wr.flush();
+//            return messages;
+        } catch (Exception e) {
+            System.out.println("error");
+        }
     }
 
     /**
@@ -73,14 +82,8 @@ public class MissionControlStatusEndpoint {
      */
     public void onEvent(@Observes StatusMessageEvent msg) throws IOException {
         UUID msgId = msg.getId();
-        Session session = peers.get(msgId);
         String message = JsonUtils.toString(msg);
-        if (session != null) {
-            session.getAsyncRemote().sendText(message);
-        } else {
-            List<String> messages = messageBuffer.computeIfAbsent(msgId, k -> new ArrayList<>());
-            messages.add(message);
-            LOG.log(Level.FINE, "No active WebSocket session was found for projectile {0}", msgId);
-        }
+        List<String> messages = messageBuffer.computeIfAbsent(msgId,k -> new ArrayList<>());
+        messages.add(message);
     }
 }
